e6d4ef90600f51ed8d3bf156ea49ec25b174708a
RainLi <lizlok@gmail.com>
2023-01-19 22:48:56 +0800

    Update l0086.c

--- l0086.c
+++ l0086.c
@@ -1,4 +1,14 @@
+/** https://leetcode.cn/problems/partition-list/discussion/
+86. 分隔链表
+中等
+670
+相关企业
+给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
 
+你应当 保留 两个分区中每个节点的初始相对位置。
+
+解决： 分两个数组就完了
+*/
 struct ListNode* partition(struct ListNode* head, int x){
     if( !head || !head->next ) return head;
 
@@ -7,7 +17,7 @@ struct ListNode* partition(struct ListNode* head, int x){
 
     struct ListNode* lHead = NULL;
     struct ListNode* lNext = NULL;
-    
+
     for(struct ListNode* p = head; p ; p = p->next ){
         if( p->val >= x){
             if( !bHead ){
